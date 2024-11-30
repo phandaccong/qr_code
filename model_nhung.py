a5fea4649812de4e94cb57c6c735a350ac6acf45
phandaccong <70447245+phandaccong@users.noreply.github.com>
2024-11-30 12:57:28 +0700

    Update model_nhung.py

--- model_nhung.py
+++ model_nhung.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("Tài liệu nhóm DP-03 Phân tích các yếu tố nguy cơ mắc bệnh nhồi máu cơ tim")
-st.markdown("[Trello nhóm DP-03 ](https://trello.com/invite/b/6720f9d7631b6c8b751d01bc/ATTI2ecc05a4567264080ce2e41c65dc6e319832DE2D/nhom-3-dat111-phan-tich-nguy-mắc-bệnh-nhồi-mau-cơ-tim)", unsafe_allow_html=True)
-st.markdown("[ Silde báo cáo ](https://www.canva.com/design/DAGXMb4wvJI/1eL07O-w5QMUukyjbnjFCg/edit?utm_content=DAGXMb4wvJI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)", unsafe_allow_html=True)
-st.markdown("[Power BI ](https://streamlit.io)", unsafe_allow_html=True)
-st.markdown("[ Báo cáo đặc tả 📖](https://docs.google.com/document/d/1p8FbSY2txyo6F3xHF-WPY0iHQbRCY9Gk/edit?usp=sharing&ouid=117659052202646370121&rtpof=true&sd=true)", unsafe_allow_html=True)
-st.markdown("[Dự đoán nguy cơ mắc bệnh tim 🩺](https://dat111-dp03-2024-heart-attack-risk.streamlit.app/)", unsafe_allow_html=True)
+st.markdown("[👉 Trello nhóm DP-03 ](https://trello.com/invite/b/6720f9d7631b6c8b751d01bc/ATTI2ecc05a4567264080ce2e41c65dc6e319832DE2D/nhom-3-dat111-phan-tich-nguy-mắc-bệnh-nhồi-mau-cơ-tim)", unsafe_allow_html=True)
+st.markdown("[👉 Silde báo cáo ](https://www.canva.com/design/DAGXMb4wvJI/1eL07O-w5QMUukyjbnjFCg/edit?utm_content=DAGXMb4wvJI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)", unsafe_allow_html=True)
+st.markdown("[👉 Power BI ](https://streamlit.io)", unsafe_allow_html=True)
+st.markdown("[👉  Báo cáo đặc tả 📖](https://docs.google.com/document/d/1p8FbSY2txyo6F3xHF-WPY0iHQbRCY9Gk/edit?usp=sharing&ouid=117659052202646370121&rtpof=true&sd=true)", unsafe_allow_html=True)
+st.markdown("[👉 Dự đoán nguy cơ mắc bệnh tim 🩺](https://dat111-dp03-2024-heart-attack-risk.streamlit.app/)", unsafe_allow_html=True)
