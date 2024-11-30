c2fa3adbd287f297c8041994e9a4d5e2dfa19512
phandaccong <70447245+phandaccong@users.noreply.github.com>
2024-11-30 12:56:20 +0700

    Update model_nhung.py

--- model_nhung.py
+++ model_nhung.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Nhúng liên kết vào Streamlit")
+st.title("Tài liệu nhóm DP-03 Phân tích các yếu tố nguy cơ mắc bệnh nhồi máu cơ tim")
 st.markdown("[Trello nhóm DP-03 ](https://trello.com/invite/b/6720f9d7631b6c8b751d01bc/ATTI2ecc05a4567264080ce2e41c65dc6e319832DE2D/nhom-3-dat111-phan-tich-nguy-mắc-bệnh-nhồi-mau-cơ-tim)", unsafe_allow_html=True)
 st.markdown("[ Silde báo cáo ](https://www.canva.com/design/DAGXMb4wvJI/1eL07O-w5QMUukyjbnjFCg/edit?utm_content=DAGXMb4wvJI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)", unsafe_allow_html=True)
 st.markdown("[Power BI ](https://streamlit.io)", unsafe_allow_html=True)
