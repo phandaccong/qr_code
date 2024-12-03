94bcf97fc4b445fe900c4e20f21758773a07d1a3
phandaccong <70447245+phandaccong@users.noreply.github.com>
2024-12-03 15:30:35 +0700

    Update model_nhung.py

--- model_nhung.py
+++ model_nhung.py
@@ -20,6 +20,6 @@ st.markdown("""
 st.markdown("[👉 Trello nhóm DP-03 📝 ](https://trello.com/invite/b/6720f9d7631b6c8b751d01bc/ATTI2ecc05a4567264080ce2e41c65dc6e319832DE2D/nhom-3-dat111-phan-tich-nguy-mắc-bệnh-nhồi-mau-cơ-tim)", unsafe_allow_html=True)
 st.markdown("[👉 Silde báo cáo 📜](https://www.canva.com/design/DAGXMb4wvJI/1eL07O-w5QMUukyjbnjFCg/edit?utm_content=DAGXMb4wvJI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)", unsafe_allow_html=True)
 st.markdown("[👉 Video câu chuyện 📽️](https://drive.google.com/file/d/1g_BydOS-o8P3mHBWUIJh8CBvXD-6Gs4d/view?usp=sharing)", unsafe_allow_html=True)
-st.markdown("[👉 Power BI 🎨](https://streamlit.io)", unsafe_allow_html=True)
+st.markdown("[👉 Power BI 🎨](https://app.powerbi.com/links/TgpGg2mXcA?ctid=447080b4-b9c6-4b0b-92fd-b543a68b4e97&pbi_source=linkShare&bookmarkGuid=0bb4f22a-7846-4e3b-a276-0c85b7e80aad)", unsafe_allow_html=True)
 st.markdown("[👉  Báo cáo đặc tả 📖](https://docs.google.com/document/d/1p8FbSY2txyo6F3xHF-WPY0iHQbRCY9Gk/edit?usp=sharing&ouid=117659052202646370121&rtpof=true&sd=true)", unsafe_allow_html=True)
 st.markdown("[👉 Dự đoán nguy cơ mắc bệnh tim 🩺](https://dat111-dp03-2024-heart-attack-risk.streamlit.app/)", unsafe_allow_html=True)
