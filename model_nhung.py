a7e11a23dc1665fddd641eaecc8f71a23375765c
phandaccong <70447245+phandaccong@users.noreply.github.com>
2024-12-11 21:43:38 +0700

    Update model_nhung.py

--- model_nhung.py
+++ model_nhung.py
@@ -11,15 +11,16 @@ st.markdown("""
             font-weight: bold; /* In đậm văn bản */
         }
     </style>
-    <p class="centered-text">+ Tập thể dục thường xuyên</p>
-    <p class="centered-text">+ Kiểm soát cân nặng</p>
-    <p class="centered-text">+ Kiểm tra huyết áp và cholesterol</p>
-    <p class="centered-text">+ Quản lý căng thẳng</p>
-    <p class="centered-text">+ Ngủ đủ giấc (7-8h)</p>
+    <p class="centered-text">Tập thể dục thường xuyên
+                            Kiểm soát cân nặng</p>
+    <p class="centered-text">Kiểm soát cân nặng</p>
+    <p class="centered-text">Kiểm tra huyết áp và cholesterol</p>
+    <p class="centered-text">Quản lý căng thẳng</p>
+    <p class="centered-text">Ngủ đủ giấc (7-8h)</p>
 """, unsafe_allow_html=True)
 st.markdown("[👉 Trello nhóm DP-03 📝 ](https://trello.com/invite/b/6720f9d7631b6c8b751d01bc/ATTI2ecc05a4567264080ce2e41c65dc6e319832DE2D/nhom-3-dat111-phan-tich-nguy-mắc-bệnh-nhồi-mau-cơ-tim)", unsafe_allow_html=True)
 st.markdown("[👉 Silde báo cáo 📜](https://www.canva.com/design/DAGXMb4wvJI/1eL07O-w5QMUukyjbnjFCg/edit?utm_content=DAGXMb4wvJI&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)", unsafe_allow_html=True)
 st.markdown("[👉 Video câu chuyện 📽️](https://drive.google.com/file/d/1g_BydOS-o8P3mHBWUIJh8CBvXD-6Gs4d/view?usp=sharing)", unsafe_allow_html=True)
-st.markdown("[👉 Power BI 🎨](https://app.powerbi.com/links/TgpGg2mXcA?ctid=447080b4-b9c6-4b0b-92fd-b543a68b4e97&pbi_source=linkShare&bookmarkGuid=0bb4f22a-7846-4e3b-a276-0c85b7e80aad)", unsafe_allow_html=True)
+st.markdown("[👉 Power BI 🎨](https://app.powerbi.com/links/lHriK8CPXu?ctid=447080b4-b9c6-4b0b-92fd-b543a68b4e97&pbi_source=linkShare&bookmarkGuid=c18b246d-81dd-41c2-a9ca-7f1ce71a8bdc)", unsafe_allow_html=True)
 st.markdown("[👉  Báo cáo đặc tả 📖](https://docs.google.com/document/d/1p8FbSY2txyo6F3xHF-WPY0iHQbRCY9Gk/edit?usp=sharing&ouid=117659052202646370121&rtpof=true&sd=true)", unsafe_allow_html=True)
 st.markdown("[👉 Dự đoán nguy cơ mắc bệnh tim 🩺](https://dat111-dp03-2024-heart-attack-risk.streamlit.app/)", unsafe_allow_html=True)
